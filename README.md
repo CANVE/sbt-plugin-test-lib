ab987d133619790ebd386b025876478d0aca2096
matan <dev.matan@gmail.com>
2015-10-15 20:06:32 +0300

    basic runner

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 `sbt run` 
 
+Note: To pull all test projects in, use `git submodule update --init`, as they are not exlicitly included
+
 ###Maintaining the list of test projects:
 
-The projects to be used for the testing are placed under `test-projects`, and they are managed as [git submodules](https://git-scm.com/docs/git-submodule).
+The projects to be used for the testing are placed under `test-projects`, and they are managed as [git submodules](https://git-scm.com/docs/git-submodule). Loosely speaking this means they are not physically included in this repo, but rather linked to. This is why you have to pull them in after cloning this repo (or possibly after pulling this repo after the list has changed), through the command mentioned above.
 
 ##Adding a project:
 
-`git submodule add <git clone link> test-projects/<project name>`. 
+Use `git submodule add <git clone link> test-projects/<project name>`. 
 
 E.g. `git submodule add git@github.com:allenai/pipeline.git test-projects/pipeline`
--- src/main/scala/sbtPluginTestlib/main.scala
+++ src/main/scala/sbtPluginTestlib/main.scala
@@ -2,23 +2,72 @@ package org.canve.sbtPluginTestLib
 
 import org.canve.util.CanveDataIO
 import java.io.{File}
+import scala.sys.process._
 
-object app extends App {
+case class Result(project: String, result: Boolean)
+
+case class AnsiFilterer(outfile: String) extends ProcessLogger {
+  
+  def buffer[T](f: ⇒ T): T = f
+  
+  def out(s: ⇒ String): Unit = {
+    print(".")  
+  }
+  
+  def err(s: ⇒ String): Unit = {
+    
+  }
+}
+
+/*
+ * Summary Output formatter
+ */
+object Summary {
+  def apply(results: Array[Result]) = {
+    println()
+    
+    val output = results map { result => 
+      Console.BOLD + "[" + 
+      Console.YELLOW + result.project + 
+      " => " +
+      Console.BOLD + (result.result match {
+        case true =>  "Ok"
+        case false => Console.RED + "Failed" + Console.YELLOW
+      }) + Console.WHITE + "]" +
+      Console.RESET
+    }
+    
+    println(output.mkString("\n"))
+    
+    println()
+  }
+}
+
+object Runner extends App {
   
   val testProjectsRoot = "test-projects"
   
-  CanveDataIO.getSubDirectories(testProjectsRoot) map { projectDirObj =>
+  val results = CanveDataIO.getSubDirectories(testProjectsRoot) map { projectDirObj =>
     val projectName = projectDirObj.getName
+    val projectPath = testProjectsRoot + File.separator + projectName 
+    print(Console.YELLOW + Console.BOLD + s"Running the sbt plugin for $projectPath..." + Console.RESET) 
     val result = injectAndTest(projectDirObj)
-    (projectName, result)
-  }
+    println(result match {
+      case true => "finished okay"
+      case false => "failed"
+    })
+    Result(projectName, result)
+  } 
   
-  def injectAndTest(projectDirObj: File) {
+  Summary(results)
+  
+  private def injectAndTest(projectDirObj: File): Boolean = {
+    // add the plugin to the project's sbt setup
     scala.tools.nsc.io.File(projectDirObj.toString + File.separator + "project/canve.sbt")
-      .writeAll("""addSbtPlugin("canve" % "sbt-plugin" % "0.0.1")""" + "\n")              
+      .writeAll("""addSbtPlugin("canve" % "sbt-plugin" % "0.0.1")""" + "\n")      
+     
+    // run sbt for the project and get the exit code
+    Process(Seq("sbt", "-Dsbt.log.noformat=true", "canve"), projectDirObj) ! (AnsiFilterer(projectDirObj.toString)) == 0
   }
-  
-  
-  
 }
 
